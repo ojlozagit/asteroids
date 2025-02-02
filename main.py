484aef169f5098e7c932419b8fa4fa2a99613703
ojlozagit <ojloza@gmail.com>
2025-02-01 23:30:08 -0500

    Added player rotation event handling with new Player() methods and constant; refactored comments for better readability for Python style guide.

--- main.py
+++ main.py
@@ -14,23 +14,32 @@ def main():
     pygame.init()
     clock = pygame.time.Clock()
     dt = 0
-    
-    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT)) # -> pygame.surface.Surface()
+
+     # screen is a Surface() object
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     player = Player( SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
     
     while True: # gameLoop
-        # we want to allow the user to exit the screen/gameLoop via the x-button
+
+        # -- Event handling --
+        # We want to allow the user to close screen via the x-button
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-            
-        screen.fill("black") # "black" is the equivalent to passing the (0,0,0) rgb tuple
+        player.update(dt)
+        # --------------------
+
+        # -- Rendering --
+        screen.fill("black") # "black" == (0,0,0) rgb tuple
         player.draw(screen)
-        pygame.display.flip() # we want to refresh the screen at the end of the gameLoop
-        dt = clock.tick(60) / 1000 # we want to pause the gameloop every 1/60 of a second, so as
-                                   # to not be rate limited by the CPU's speed.
-                                   # This time passed should then be saved as the delta (i.e.,
-                                   # time passed since last frame/drawcall)
+        pygame.display.flip() # used to refresh screen state
+        # ---------------
+
+        # We want to pause the gameloop every 1/60 of a second, so as
+        # to not be rate limited by the CPU's speed.
+        # This time passed should then be saved as the delta (i.e.,
+        # time passed since last frame/drawcall)
+        dt = clock.tick(60) / 1000
 
 if __name__ == "__main__":
     main()
