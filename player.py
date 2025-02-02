c57c04eed5ddd9b3424a3acd2cc1a2d67744a7d4
ojlozagit <ojloza@gmail.com>
2025-02-02 00:59:39 -0500

    Added the ability to move to the Player class; refactored Player class organization so that the more abstract interfaces are towards the top

--- player.py
+++ player.py
@@ -6,6 +6,21 @@ class Player(CircleShape):
         super().__init__(x, y, PLAYER_RADIUS)
         self.rotation = 0
 
+    def draw(self, screen):
+        return pygame.draw.polygon(screen, "white", self.triangle(), 2)
+
+    def update(self, dt):
+        keys = pygame.key.get_pressed()
+
+        if keys[pygame.K_w]:
+            self.move(dt)
+        if keys[pygame.K_a]:
+            self.rotate(-dt)
+        if keys[pygame.K_s]:
+            self.move(-dt)
+        if keys[pygame.K_d]:
+            self.rotate(dt)
+
     def triangle(self):
         forward = pygame.Vector2(0, 1).rotate(self.rotation)
         right   = (pygame.Vector2(0, 1).rotate(self.rotation + 90)
@@ -15,17 +30,10 @@ class Player(CircleShape):
         c = self.position - forward * self.radius + right
         return [a, b, c]
 
-    def draw(self, screen):
-        # (surface, color, points, width) --> rect
-        return pygame.draw.polygon(screen, "white", self.triangle(), 2)
-
     def rotate(self, dt):
          self.rotation += PLAYER_TURN_SPEED * dt
 
-    def update(self, dt):
-        keys = pygame.key.get_pressed()
-
-        if keys[pygame.K_a]:
-            self.rotate(-dt)
-        if keys[pygame.K_d]:
-            self.rotate(dt)
+    def move(self, dt):
+        forward = pygame.Vector2(0, 1).rotate(self.rotation)
+        displacement = PLAYER_SPEED * dt
+        self.position += forward * displacement
